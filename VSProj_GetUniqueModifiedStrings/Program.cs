0c68e5ffe7b5fb73629e5e8dafbb549f34461ced
IvanKolbasyuk <ikolbasyukitonit@gmail.com>
2020-08-15 12:55:36 +0300

    Base project

---
+++ VSProj_GetUniqueModifiedStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace GetUniqueModifiedStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.AutoFlush = true;
+            Debug.WriteLine("test2" + DateTime.Now.ToString("s"));
+        }
+    }
+}
